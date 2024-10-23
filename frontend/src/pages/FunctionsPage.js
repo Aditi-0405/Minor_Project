c054f294ac7842257b98a5f2110caeae5e205eb0
Aditi-0405 <somyaaditi02@gmail.com>
2024-10-23 18:57:27 +0530

    added select method option

--- frontend/src/pages/FunctionsPage.js
+++ frontend/src/pages/FunctionsPage.js
@@ -4,6 +4,7 @@ import styles from '../styles/FunctionsPage.module.css';
 
 const FunctionsPage = () => {
   const [selectedFunction, setSelectedFunction] = useState('');
+  const [selectedMethod, setSelectedMethod] = useState('');
   const [optimizationResult, setOptimizationResult] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
@@ -12,9 +13,13 @@ const FunctionsPage = () => {
     setSelectedFunction(event.target.value);
   };
 
+  const handleMethodSelect = (event) => {
+    setSelectedMethod(event.target.value); 
+  };
+
   const handleOptimize = async () => {
-    if (!selectedFunction) {
-      setError('Please select a function to optimize');
+    if (!selectedFunction || !selectedMethod) {
+      setError('Please select a function and a method to optimize');
       return;
     }
 
@@ -22,7 +27,10 @@ const FunctionsPage = () => {
       setLoading(true);
       setError('');
       const res = await axios.get("http://127.0.0.1:8080/api/optimize", {
-        params: { function: selectedFunction } 
+        params: { 
+          function: selectedFunction,
+          method: selectedMethod 
+        } 
       });
       console.log(res.data);  
       setOptimizationResult(res.data);
@@ -36,12 +44,20 @@ const FunctionsPage = () => {
 
   return (
     <div className={styles.container}>
-      <h1>Select a Function to Optimize</h1>
+      <h1>Select a Function and Method to Optimize</h1>
+
       <select onChange={handleFunctionSelect} value={selectedFunction} className={styles.functionSelect}>
         <option value="">--Select a Function--</option>
         <option value="g1">g1: "(3 * x1) - cos(x2 * x3) - 0.5"</option>
         <option value="g2">g2: "x1^2 - 81 * (x2 + 0.1)^2 + sin(x3) + 1.06"</option>
         <option value="g3">g3: "exp(-x1 * x2) + 20 * x3 + (10 * π - 3) / 3"</option>
+      </select>
+
+      <select onChange={handleMethodSelect} value={selectedMethod} className={styles.methodSelect}>
+        <option value="">--Select a Method--</option>
+        <option value="method1">gradient descent</option>
+        <option value="method2">Newton-Raphson</option>
+        <option value="method3">steepest gradient descent</option>
       </select>
 
       <button onClick={handleOptimize} className={styles.optimizeButton} disabled={loading}>
